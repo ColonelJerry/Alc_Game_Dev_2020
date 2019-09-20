d9cc44f9bf410aa4ccf76122497b9cff12603617
Leo Montgomery <leo.m922@stu.nebo.edu>
2019-09-20 10:31:58 -0600

    Challenge and cleanup

--- CreateWithCode/Prototype 1/Assets/Scripts/playerControl.cs
+++ CreateWithCode/Prototype 1/Assets/Scripts/playerControl.cs
@@ -4,24 +4,18 @@ using UnityEngine;
 
 public class playerControl : MonoBehaviour
 {
-    public float speed = 20f;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // playercontroller private variables
+    private float speed = 20f;
+    private float turnSpeed = 25f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Update is called once per frame
     void Update()
     {
+        // This code moves the vehicle backwards and forwards as well as turning it
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-
-        //Move vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
