36514f20f0ba659d40bb0e17a554e0705aa106f1
Leo Montgomery <leo.m922@stu.nebo.edu>
2019-10-04 10:14:39 -0600

    Plane game

--- CreateWithCode/Prototype 1/Assets/Scripts/planegame.cs
+++ CreateWithCode/Prototype 1/Assets/Scripts/planegame.cs
@@ -8,29 +8,38 @@ public class planegame : MonoBehaviour
     public float rotationSpeed;
     public float verticalInput;
     public float horizontalInput;
+    private bool engine=false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // get the user's vertical input
-        verticalInput = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            engine = !engine;
+        }
+        if (engine == true)
+        {
+            // move the plane forward at a constant rate
+            transform.Translate(Vector3.forward * speed);
+            
+        }
 
-        //get the user's horizontal input
-        horizontalInput = Input.GetAxis("Horizontal");
+            // get the user's vertical input
+            verticalInput = Input.GetAxis("Vertical");
 
-        // move the plane forward at a constant rate
-        transform.Translate(Vector3.forward * speed);
+            //get the user's horizontal input
+            horizontalInput = Input.GetAxis("Horizontal");
 
-        // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+            // tilt the plane up/down based on up/down arrow keys
+            transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
 
-        // tilt the plane left/right based on left/right arrow keys
-        transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime * horizontalInput);
+            // tilt the plane left/right based on left/right arrow keys
+            transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime * horizontalInput);
+        }
     }
-}
